51b59eb938a26b9f3a9ce1f25015a205a9e08bca
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-01-15 15:40:21 +0600

    added functionality to get all  usage log

--- src/modules/usage-log/log.controller.ts
+++ src/modules/usage-log/log.controller.ts
@@ -11,3 +11,13 @@ const createUsageLog: RequestHandler = async (req, res) => {
         res.send({ message: 'log creation error', error })
     }
 }
+
+const getUsageLogs: RequestHandler = async (req, res) => {
+    try {
+        const log = await prisma.usageLog.findMany()
+
+        res.send({ message: "logs", data: log })
+    } catch (error) {
+        res.send({ message: 'log creation error', error })
+    }
+}
